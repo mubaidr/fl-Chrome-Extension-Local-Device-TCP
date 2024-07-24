fbea35057d25e9120103cc2b5e732470be4c8e52
Muhammad Ubaid Raza <mubaidr@gmail.com>
2024-07-24 13:35:25 +0500

    refactor: Update manifest files to remove unnecessary permissions

--- manifest.firefox.config.ts
+++ manifest.firefox.config.ts
@@ -57,4 +57,8 @@ export default defineManifest(async (env) => ({
     32: 'src/assets/logo.png',
     128: 'src/assets/logo.png',
   },
+  content_security_policy: {
+    extension_pages: "default-src 'self'",
+    sandbox: "default-src 'self'",
+  },
 }))
